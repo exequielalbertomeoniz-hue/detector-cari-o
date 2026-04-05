e4a9af6c504af4904344b535930da9d3bdce835b
Az4z3l.- <264879027+exequielalbertomeoniz-hue@users.noreply.github.com>
2026-04-05 17:27:18 -0300

    cariño.py with new messages

--- detector-cariño.py
+++ detector-cariño.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("💖 Detector de Cariño")
+st.title("💖 Detector de Cariño By Exequielito")
 
-st.write("Esto es importante… respondé con sinceridad 😌")
+st.write("Esto es importante… respondeme con sinceridad 😌")
 
 nombre = st.text_input("¿Cómo te llamás?")
 altura = st.number_input("¿Cuánto medís (en metros)?", step=0.01)
@@ -14,8 +14,8 @@ if st.button("Descubrir resultado ❤️"):
         st.success("Resultado final:")
         st.write("El contenedor es menor a la cantidad de cariño 💖")
         st.write(f"{nombre} mide {altura} metros.")
-        st.write(f"Por ende, {nombre} mide {altura*100:.0f} cm de pura ternura ✨")
+        st.write(f"Por ende, {nombre} mide {altura*100:.0f} cm de pura belleza y ternura, terremoto orgasmico de facha (L) ✨")
         
         st.balloons()
     else:
-        st.warning("Tenés que completar todo 😄")
+        st.warning("Tenés que completar todo, si no, no vale  -_-" - TRAMPOSA! 😄")
